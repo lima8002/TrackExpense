c0a841f7c5c4a4291c08c140152b1a92874224d2
Eduardo Lima <lima8002@gmail.com>
2023-08-28 14:39:07 -0300

    basic structure

---
+++ screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+function AllExpenses() {
+  return (
+    <View>
+      <Text>AllExpenses</Text>
+    </View>
+  )
+}
+
+export default AllExpenses;
