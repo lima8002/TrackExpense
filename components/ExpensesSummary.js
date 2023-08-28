7f6e086c5453152e6b380f222983f0c8fef854b9
Eduardo Lima <lima8002@gmail.com>
2023-08-28 16:42:31 -0300

    desconstructing into components

---
+++ components/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>ExpensesSummary</Text>
+    </View>
+  )
+}
+
+export default ExpensesSummary;
