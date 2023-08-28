562ce9ee449444a4d46da95141a49f59c66a6dd2
Eduardo Lima <lima8002@gmail.com>
2023-08-28 16:49:11 -0300

    coding ExpensesSummary.js

--- components/ExpensesSummary.js
+++ components/ExpensesSummary.js
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-function ExpensesSummary() {
+function ExpensesSummary({ expenses, periodName }) {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount
+ }, 0);
+
   return (
     <View>
-      <Text>ExpensesSummary</Text>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
     </View>
-  )
+  );
 }
 
 export default ExpensesSummary;
